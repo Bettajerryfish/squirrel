198eb6a46b9858b9fdcaa353780bbc48aa560710
jerry <jc5298@columbia.edu>
2019-12-01 22:24:10 +0000

    add the urls.py

--- finalproject/map/views.py
+++ finalproject/map/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Squirrel
+
+
+def squirrels_map(request):
+    sightings=Squirrel.objects.all()[:100]
+
+    return render(request, 'map/squirrels_map.html',{'sightings':sightings})
+
 # Create your views here.
