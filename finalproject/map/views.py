4fba5585ac6f3c2aec1e6c4df993d5d7cfc865ad
jerry <jc5298@columbia.edu>
2019-12-01 22:29:29 +0000

    fixed type

--- finalproject/map/views.py
+++ finalproject/map/views.py
@@ -6,6 +6,6 @@ from .models import Squirrel
 def squirrels_map(request):
     sightings=Squirrel.objects.all()[:100]
 
-    return render(request, 'map/squirrels_map.html',{'sightings':sightings})
+    return render(request, 'map/squirrel_map.html',{'sightings':sightings})
 
 # Create your views here.
