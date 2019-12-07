b81a1eb5644e9b2589651b4294e5a4b8f5b47ddc
Bettajerryfish <jc5298@columabi.edu>
2019-12-07 03:35:43 +0000

    fix the template dir

--- finalproject/finalproject/settings.py
+++ finalproject/finalproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'finalproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'map', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
