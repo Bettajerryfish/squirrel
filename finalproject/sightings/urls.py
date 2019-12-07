6e26fe83eeb2b3c16b59cdb0db4e30d5ba42eaed
Zhe Wang <zw2633@columbia.edu>
2019-12-07 23:58:35 +0000

    Update urls.py and views.py

--- finalproject/sightings/urls.py
+++ finalproject/sightings/urls.py
@@ -6,12 +6,9 @@ app_name='sightings'
 
 urlpatterns = [
 
-            path('', views.index, name = 'index'),
-            
+            path('', views.index, name = 'home'),
             path('add/', views.add, name = 'add'),
             path('stats/', views.stats, name = 'stats'),
             path('<unique_squirrel_id>/', views.update, name = "update"),
-            # path('<str:unique_squirrel_id>/', views.edit, name = 'edit'),
-            # path('<str:unique_squirrel_id>/delete/', views.delete, name = 'delete'),
 
              ]
--- finalproject/sightings/views.py
+++ finalproject/sightings/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse,HttpResponseRedirect
 from map.models import Squirrel
 from .forms import SquirrelForm
 # Create your views here.
-def index (request):
+def home (request):
     sightings=Squirrel.objects.all()
     context={'sightings':sightings,} 
-    return render(request,"sightings/index.html",context)
+    return render(request,"sightings/home.html",context)
 
 def stats(request):
     Total_Num=Squirrel.objects.all().count()
@@ -31,24 +31,13 @@ def add(request):
     return render(request,'sightings/add.html',context)
 
 
-#def update(request, unique_squirrel_id):
- #   sighting = get_object_or_404(Squirrel,unique_squirrel_id=unique_squirrel_id)
-  #  if request.method=="POST":
-   #     form=SquirrelForm(request.POST,instance=sighting)
-    #    if form.is_valid():
-     #       form.save()
-      #      return redirect('sightings:index')
- #   else:
-  #      form=SquirrelForm(instance=sighting)
-  #  context={'form':form}
-   # return render(request,'sightings/update.html',context)
 def update(request, unique_squirrel_id):
-    edit_sighting = get_object_or_404(Squirrel, unique_squirrel_id=unique_squirrel_id)
+    up_sighting = get_object_or_404(Squirrel, unique_squirrel_id=unique_squirrel_id)
     if request.method == "POST":
-        form = SquirrelForm(request.POST, instance=edit_sighting)
+        form = SquirrelForm(request.POST, instance=up_sighting)
         if form.is_valid():
             form.save()
             return redirect("/sightings/")
-    form = SquirrelForm(instance=edit_sighting)
+    form = SquirrelForm(instance=up_sighting)
     return render(request, "sightings/update.html", {"form": form, "unique_squirrel_id":unique_squirrel_id})
 # Create your views here.
